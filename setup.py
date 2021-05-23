1b83efc9b76cf6611567a505af86f0c721e691e5
Steven Zindel <zindel.steven@kpmg.nl>
2021-05-23 11:02:38 +0200

    changed minimum version req python

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name = 'funcfactory',         # How you named your package folder (MyLib)
     packages = ['funcfactory'],   # Chose the same as "name"
-    python_requires=">=3.6",
-    version = '0.1.4',      # Start with a small number and increase it with every change you make
+    python_requires=">=3.7",
+    version = '0.1.5',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -20,15 +20,15 @@ setup(
     author_email = 'steven.zindel@gmail.com', # Type in your E-Mail
     url = 'https://github.com/szindel/FuncFactory',   # Provide either the link to your github or to your website
     download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-    keywords = ['FuncFactory', 'Functions', 'Production'],   # Keywords that define your package best
+    keywords = ['FuncFactory', 'Functions', 'Production Code'],   # Keywords that define your package best
     classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
         'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
